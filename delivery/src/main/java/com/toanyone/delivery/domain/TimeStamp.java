22931807204b5ee94fc0efedfa5b753b6f0bb634
venus01 <geumsung7769@naver.com>
2025-03-13 23:11:32 +0900

    feat : TimeStamp 구현
    추상 클래스 TimeStamp를 생성하였음. 각 도메인 엔티티들은
    TimeStamp를 상속받아 생성, 수정, 삭제에 관한 정보를 설정하게 됨
    
    이슈 해결 : #8

---
+++ delivery/src/main/java/com/toanyone/delivery/domain/TimeStamp.java
@@ -0,0 +1,34 @@
+package com.toanyone.delivery.domain;
+
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class TimeStamp {
+
+    @CreatedDate
+    protected LocalDateTime createdAt;
+
+    @CreatedBy
+    protected Integer createdBy;
+
+    @LastModifiedDate
+    protected LocalDateTime updatedAt;
+
+    @LastModifiedBy
+    protected Integer updatedBy;
+
+    protected LocalDateTime deletedAt;
+
+    protected Integer deletedBy;
+}
