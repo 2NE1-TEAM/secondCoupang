074ccf067d0f0c3d6993e2e806f9e7c952767f26
jnjongjeon <jeonij23@gmail.com>
2025-03-15 19:23:25 +0900

    feat: 스토어 생성, 삭제, 단건 조회 로직 구현

---
+++ store/src/main/java/com/toanyone/store/presentation/dto/StoreFindResponseDto.java
@@ -0,0 +1,33 @@
+package com.toanyone.store.presentation.dto;
+
+import com.toanyone.store.common.util.PhoneNumberUtils;
+import com.toanyone.store.domain.model.DetailAddress;
+import com.toanyone.store.domain.model.Location;
+import com.toanyone.store.domain.model.Store;
+import com.toanyone.store.domain.model.StoreType;
+import lombok.*;
+
+@Getter
+public class StoreFindResponseDto {
+    private Long storeId;
+    private String storeName;
+    private StoreType storeType;
+    private Location location;
+    private DetailAddress detailAddress;
+    private Long hubId;
+    private String hubName;
+    private String telephone;
+
+    public static StoreFindResponseDto of(Store store, Long hubId, String hubName) {
+        StoreFindResponseDto storeFindResponseDto = new StoreFindResponseDto();
+        storeFindResponseDto.storeId = store.getId();
+        storeFindResponseDto.storeName = store.getStoreName();
+        storeFindResponseDto.storeType = store.getStoreType();
+        storeFindResponseDto.location = store.getLocation();
+        storeFindResponseDto.detailAddress = store.getDetailAddress();
+        storeFindResponseDto.hubName = hubName;
+        storeFindResponseDto.telephone = PhoneNumberUtils.formatPhoneNumber(store.getTelephone()); // 전화번호 "-" 추가
+        storeFindResponseDto.hubId = hubId;
+        return storeFindResponseDto;
+    }
+}
