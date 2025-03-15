83ec8963a0c499da5a62e26e50f0d648113547e4
jhwan <jhwan.kim95@gmail.com>
2025-03-15 18:09:27 +0900

    feat sign-in 기능 추가

---
+++ user/src/main/java/com/toanyone/user/user/infrastructure/JwtUtil.java
@@ -0,0 +1,38 @@
+package com.toanyone.user.user.infrastructure;
+
+import com.toanyone.user.user.domain.UserRole;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+
+    @Value("${spring.application.name}")
+    private String issuer;
+
+    @Value("${service.jwt.secret-key}")
+    private String secretKey;
+
+    private final Long EXPIRE_TIME = 60 * 60 * 1000L;
+    private final String TOKEN_PREFIX = "Bearer ";
+
+
+
+    public String generateToken(Long userId, UserRole userRole) {
+
+        return   TOKEN_PREFIX+ Jwts.builder()
+                .claim("userId", userId)
+                .claim("userRole", userRole)
+                .issuer(issuer)
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis()+EXPIRE_TIME))
+                .signWith(SignatureAlgorithm.HS512, secretKey)
+                .compact();
+
+    }
+
+}
