d60fdbae83c601c14923c82cf127edb5aecee04c
jnjongjeon <jeonij23@gmail.com>
2025-03-17 12:53:49 +0900

    fix: FeingnClient로 허브 정보 얻어올 때 헤더값 사용을 위해 ResponseEntity로 반환 타입 수정

--- store/src/main/java/com/toanyone/store/infrastructure/client/HubClient.java
+++ store/src/main/java/com/toanyone/store/infrastructure/client/HubClient.java
@@ -3,11 +3,13 @@ package com.toanyone.store.infrastructure.client;
 import com.toanyone.store.infrastructure.client.dto.HubResponseDto;
 import com.toanyone.store.presentation.dto.SingleResponse;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @FeignClient(name = "hub-service", url = "http://localhost:9000")
 public interface HubClient {
+
     @GetMapping("/hubs/{hubId}")
-    SingleResponse<HubResponseDto> getHubById(@PathVariable("hubId") Long hubId);
+    ResponseEntity<SingleResponse<HubResponseDto>> getHubById(@PathVariable("hubId") Long hubId);
 }
