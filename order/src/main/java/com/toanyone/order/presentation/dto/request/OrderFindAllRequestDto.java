ff76c98d304b85feb0dabf3ee5129340dd2cf4a8
woo-lala <hyunj2034@naver.com>
2025-03-18 17:20:30 +0900

    feat: 사용자별 주문 조회 구현

---
+++ order/src/main/java/com/toanyone/order/presentation/dto/request/OrderFindAllRequestDto.java
@@ -0,0 +1,30 @@
+package com.toanyone.order.presentation.dto.request;
+
+import com.toanyone.order.presentation.dto.SortType;
+import com.toanyone.order.presentation.dto.response.OrderFindAllResponseDto;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+import static com.toanyone.order.common.CursorConstants.DEFAULT_SIZE;
+import static com.toanyone.order.common.CursorConstants.DEFAULT_SORT_TYPE;
+
+@Getter
+public class OrderFindAllRequestDto {
+
+    private int size;
+    private Long cursorId;
+    private LocalDateTime timestamp;
+    private SortType sortType;
+
+    @Builder
+    public OrderFindAllRequestDto(Integer size, Long cursorId, LocalDateTime timestamp, SortType sortType) {
+        this.size = (size != null) ? size : DEFAULT_SIZE;
+        this.cursorId = cursorId;
+        this.timestamp = timestamp;
+        this.sortType = (sortType != null) ? sortType : DEFAULT_SORT_TYPE;
+    }
+
+}
