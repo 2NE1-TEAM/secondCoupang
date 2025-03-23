950bdc667cf68f0aec20133dec397c6df1d72eb1
Allrace <jeonij23@gmail.com>
2025-03-23 20:09:35 +0900

    feat: 예외처리 추가

--- store/src/main/java/com/toanyone/store/domain/exception/CustomException.java
+++ store/src/main/java/com/toanyone/store/domain/exception/CustomException.java
@@ -1,8 +1,10 @@
 package com.toanyone.store.domain.exception;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 
 @AllArgsConstructor
+@Getter
 public class CustomException extends RuntimeException {
     private String message;
 }
