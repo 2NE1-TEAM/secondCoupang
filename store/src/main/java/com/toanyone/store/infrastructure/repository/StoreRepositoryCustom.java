2f473013d9aca4671a63bdec1052f66599366343
Allrace <jeonij23@gmail.com>
2025-03-23 20:16:06 +0900

    fix: 커서페이징 누락 데이터 발견으로 인해 쿼리 수정, 권한 어노테이션 추가

--- store/src/main/java/com/toanyone/store/infrastructure/repository/StoreRepositoryCustom.java
+++ store/src/main/java/com/toanyone/store/infrastructure/repository/StoreRepositoryCustom.java
@@ -1,11 +1,9 @@
 package com.toanyone.store.infrastructure.repository;
 
-import com.toanyone.store.domain.model.Store;
 import com.toanyone.store.presentation.dto.CursorPage;
+import com.toanyone.store.presentation.dto.StoreFindResponseDto;
 import com.toanyone.store.presentation.dto.StoreSearchRequest;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 
 public interface StoreRepositoryCustom {
-    CursorPage<Store> search(StoreSearchRequest storeSearchRequest, String sortBy, String direction, int size);
+    CursorPage<StoreFindResponseDto> search(StoreSearchRequest storeSearchRequest, String sortBy, String direction, int size);
 }
