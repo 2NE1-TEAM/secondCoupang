6b1b2dcd1bd3bfc22195ba8d3b58b5aeb13ec813
Allrace <jeonij23@gmail.com>
2025-03-24 16:33:49 +0900

    feat: 유레카 클라이언트 추가

--- store/src/main/java/com/toanyone/store/common/config/feignConfig/FeignConfig.java
+++ store/src/main/java/com/toanyone/store/common/config/feignConfig/FeignConfig.java
@@ -1,6 +1,8 @@
 package com.toanyone.store.common.config.feignConfig;
 
+import com.toanyone.store.common.config.interceptor.UserInfoHeaderInterceptor;
 import com.toanyone.store.infrastructure.configuration.HubClientErrorDecoder;
+import feign.RequestInterceptor;
 import feign.codec.ErrorDecoder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,5 +12,10 @@ public class FeignConfig {
     @Bean
     public ErrorDecoder errorDecoder() {
         return new HubClientErrorDecoder();
+    }
+
+    @Bean
+    public RequestInterceptor userInfoHeaderInterceptor(UserInfoHeaderInterceptor interceptor) {
+        return interceptor;
     }
 }
