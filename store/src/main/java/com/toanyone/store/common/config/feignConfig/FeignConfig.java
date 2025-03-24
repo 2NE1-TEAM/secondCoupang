b4d02ad479d77affd9b4c156d1066a0c516085ec
Allrace <jeonij23@gmail.com>
2025-03-24 16:49:04 +0900

    fix: userInfoHeaderInterceptor 빈 제거

--- store/src/main/java/com/toanyone/store/common/config/feignConfig/FeignConfig.java
+++ store/src/main/java/com/toanyone/store/common/config/feignConfig/FeignConfig.java
@@ -1,8 +1,6 @@
 package com.toanyone.store.common.config.feignConfig;
 
-import com.toanyone.store.common.config.interceptor.UserInfoHeaderInterceptor;
 import com.toanyone.store.infrastructure.configuration.HubClientErrorDecoder;
-import feign.RequestInterceptor;
 import feign.codec.ErrorDecoder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,10 +10,5 @@ public class FeignConfig {
     @Bean
     public ErrorDecoder errorDecoder() {
         return new HubClientErrorDecoder();
-    }
-
-    @Bean
-    public RequestInterceptor userInfoHeaderInterceptor(UserInfoHeaderInterceptor interceptor) {
-        return interceptor;
     }
 }
